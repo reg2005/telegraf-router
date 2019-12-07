2584b06cd4ac8fcb3489c61dcb101033747020ef
Lev <levz0r@gmail.com>
2019-12-08 01:14:14 +0200

    Call errorCb with original error (fixed wrong curly brackets).

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ class Router {
       await this.router.resolve(data);
     } catch (e) {
       console.error("Router exception", e.message);
-      this.errorCb({ data, e });
+      this.errorCb(data, e);
     }
   }
 
